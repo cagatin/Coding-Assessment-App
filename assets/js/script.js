2330b00de64121d00ef99ad8c446c5d9d0b32b7b
Felix Cagatin <cagatingilbert@gmail.com>
2022-06-08 13:40:18 -0700

    Added a questions object to store question-answer pairs

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,46 @@ let answersContainer = document.querySelector('#answers-container');
 
 let time;
 
-let answer = ["Possible Answer 1", "Possible Answer 2", "Possible Answer 3", "Possible Answer 4",]
-let questions = ["Placeholder Question"]
+// let answer = ["Possible Answer 1", "Possible Answer 2", "Possible Answer 3", "Possible Answer 4",]
+// let questions = ["Placeholder Question"]
+
+let questions = [
+    {
+        question: "What does the Array.slice() method do?",
+        answers: [
+            {
+                text: "Adds an element to the end of an array.",
+                answer: false
+            },
+            {
+                text: "Removes and returns the first element in an array.",
+                answer: false
+            },
+            {
+                text: "Merges two arrays together.",
+                answer: false,
+            },
+            {
+                text: "Returns a shallow copy of a specified portion of an array.",
+                answer: true
+            }
+        ]
+    }
+]
+
 
 function displayAnswers() {
-    for (let i = 0; i < answer.length; i++) {
+    let answers = questions[0].answers;
+    for (let i = 0; i < answers.length; i++) {
         let newBtn = document.createElement('button');
         newBtn.setAttribute('class', 'answer-button');
-        newBtn.textContent = answer[i];
+        newBtn.textContent = answers[i].text;
         answersContainer.appendChild(newBtn);
     }
 }
 
 function displayQuestion() {
-    questionPara.textContent = questions[0];
+    questionPara.textContent = questions[0].question;
 }
 
 function init() {
